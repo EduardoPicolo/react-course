f698ccb4c56ba48a265428b7521e51dc759716a3
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-16 10:30:43 -0300

    29. Card list component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component'
+
 import './App.css';
 
 class App extends Component {
-   constructor() {
-      super();
+  constructor() {
+    super();
 
-      this.state = {
-         monsters: [
-            {
-               name: 'Frankenstein',
-               id: 1
-            },
-            {
-               name: 'Dracula',
-               id: 2
-            },
-            {
-               name: 'Zombie',
-               id: 3
-            }
-         ]
-      }
-   }
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 1
+        },
+        {
+          name: 'Dracula',
+          id: 2
+        },
+        {
+          name: 'Zombie',
+          id: 3
+        }
+      ]
+    }
+  }
 
-   componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/users')
-         .then(response => response.json())
-         .then(json => this.setState({ monsters: json }))
-         .catch(error => console.log(error))
-   }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => this.setState({ monsters: json }))
+      .catch(error => console.log(error))
+  }
 
-   render() {
-      return (
-         <div className="App">
-            {
-               this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-            }
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div className="App">
+        <CardList name="Edu">
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+          }
+        </CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
