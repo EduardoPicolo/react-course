ec21eeb387e1c1e0b68d6b5b8d4e911583bf4cf6
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-16 10:51:51 -0300

    31. Card component

--- src/App.js
+++ src/App.js
@@ -36,10 +36,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Edu">
-          {
-            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-          }
+        <CardList monsters={this.state.monsters}>
         </CardList>
       </div>
     );
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './card.styles.css'
+
+export const Card = props => (
+  <div className="card-container">
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}></img>
+    <h1>{props.monster.name}</h1>
+    <p>{props.monster.email}</p>
+  </div>
+)
