28c2e22d55296b84fdc2588fe33d3e7aec2dfa11
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-16 13:15:55 -0300

    Correcting typo

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,41 +6,41 @@ import { SearchBox } from "./components/search-box/search-box.component";
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+   constructor() {
+      super();
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    }
-  }
+      this.state = {
+         monsters: [],
+         searchField: ''
+      }
+   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(json => this.setState({ monsters: json }))
-      .catch(error => console.log(error))
-  }
+   componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+         .then(response => response.json())
+         .then(json => this.setState({ monsters: json }))
+         .catch(error => console.log(error))
+   }
 
-  handleChange = e => {
-    this.setState({ searchField: e.target.value })
-  }
+   handleChange = e => {
+      this.setState({ searchField: e.target.value })
+   }
 
-  render() {
-    const { monsters, searchField } = this.state
-    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+   render() {
+      const { monsters, searchField } = this.state
+      const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
 
-    return (
-      <div className="App">
-        <h1>Monsters rolodex</h1>
-        <SearchBox
-          placeholder='Search monsters'
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters}></CardList>
-      </div>
-    );
-  }
+      return (
+         <div className="App">
+            <h1>Monsters rolodex</h1>
+            <SearchBox
+               placeholder='Search'
+               handleChange={this.handleChange}
+            />
+            <CardList monsters={filteredMonsters}></CardList>
+         </div>
+      );
+   }
 }
 
 export default App;
