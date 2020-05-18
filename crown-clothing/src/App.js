1432cf87aded542cc5399cae4e0811ed3589eebe
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-18 07:16:12 -0300

    64. Homepage and Directory components

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage/homepage.component'
 
 function App() {
-   return (
-      <HomePage>
-         Hello world
-      </HomePage>
-   );
+  return (
+    <HomePage>
+      Hello world
+    </HomePage>
+  );
 }
 
 export default App;
