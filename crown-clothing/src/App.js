3fcaffd263e4d53780982a7d30d6c07f57c3d195
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-18 09:49:39 -0300

    73. Header component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Switch, Route } from "react-router-dom";
-import './App.css';
+import { Switch, Route } from "react-router-dom"
+import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
