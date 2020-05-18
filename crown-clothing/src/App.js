47867cc312edb7ffefebe45f552156b54c26b245
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-18 10:20:11 -0300

    76. Sign In component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -5,6 +5,7 @@ import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
