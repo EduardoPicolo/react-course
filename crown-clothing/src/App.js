d4c11be3c48e11b55e187c3e35e1d0c03aa7a8ee
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-19 11:14:12 -0300

    90. Storing user data in Firebase

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      } else {
+        this.setState({ currentUser: null })
+      }
     })
   }
 
---
+++ crown-clothing/src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+import './sign-up.styles.scss'
