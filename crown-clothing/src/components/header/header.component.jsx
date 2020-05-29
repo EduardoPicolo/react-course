87d5a21706bd76ee24a9f4b3d99c01a9a7faed7b
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-29 06:01:14 -0300

    110. Implementing Redux in Cart

--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='name' />
@@ -30,12 +30,16 @@ const Header = ({ currentUser }) => (
         )}
       <CartIcon />
     </div>
-    <CartDropdown />
+    {hidden ?
+      null : <CartDropdown />
+    }
   </div>
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  // currentUser: state.user.currentUser
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header)
