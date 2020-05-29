7452b6ba459aa07bc174c74d5dcfc4f23e6c9d59
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-29 08:46:31 -0300

    119. Reselect Library

--- crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crown-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custon-button.component'
 import CartItem from '../cart-item/cart-item.component'
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss'
 
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(
