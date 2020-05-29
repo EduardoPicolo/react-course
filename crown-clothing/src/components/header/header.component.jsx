e880ad72a4ba79d9e5d40c1ad288936e9f0ff28e
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-29 05:24:50 -0300

    108. CartIcon component

--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from "react-redux"
 import { auth } from '../../firebase/firebase.utils'
 
 import { ReactComponent as Logo } from "../../assets/crown.svg"
+import CartIcon from '../cart-icon/cart-icon.component'
 
 import './header.styles.scss'
 
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
             SIGN IN
           </Link>
         )}
+      <CartIcon />
     </div>
   </div>
 )
