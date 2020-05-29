9d1f2832f8cd3a8638b052b77d01a479098110cf
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-29 03:53:45 -0300

    103. Setting up Redux: Part 1

--- crown-clothing/src/index.js
+++ crown-clothing/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from "react-redux"
 
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
