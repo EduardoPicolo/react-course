7390da7305075522105150b6bb207aaeab2069b6
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-30 01:28:55 -0300

    130. Redux persist

--- crown-clothing/src/index.js
+++ crown-clothing/src/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux"
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './redux/store'
+import { store, persistor } from './redux/store'
+
 
 import './index.css'
 import App from './App'
@@ -12,7 +14,9 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
