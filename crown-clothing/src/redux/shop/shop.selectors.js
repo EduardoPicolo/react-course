45282701f7cb1f7e8f7ddf0d2c44689d44fbc475
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-30 03:23:24 -0300

    138. Data normalization

--- crown-clothing/src/redux/shop/shop.selectors.js
+++ crown-clothing/src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from 'reselect'
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}
-
 const selectShop = state => state.shop
 
 export const selectShopCollections = createSelector(
@@ -17,5 +9,5 @@ export const selectShopCollections = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
   [selectShopCollections],
-  collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+  collections => collections[collectionUrlParam]
 )
