6d224ad000ccb642e15bccfbe8ad7d6df1c29de2
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-30 03:52:47 -0300

    Added navigation to collections preview

--- crown-clothing/src/components/collection-preview/collection-preview.jsx
+++ crown-clothing/src/components/collection-preview/collection-preview.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { withRouter } from "react-router-dom";
 
 import CollectionItem from '../collection-item/collection-item.component'
 
 import './collection-preview.scss'
 
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, routeName, history, match }) => (
   <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
+    <h1 className="title" onClick={() => history.push(`${match.path}/${routeName}`)}>{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .slice(0, 4)
@@ -18,4 +19,4 @@ const CollectionPreview = ({ title, items }) => (
   </div>
 )
 
-export default CollectionPreview
+export default withRouter(CollectionPreview)
