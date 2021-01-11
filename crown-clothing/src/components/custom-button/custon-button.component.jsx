ac2b7ad2a6de040f82c871b4f75cfc2014ee48fe
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-01-11 17:17:38 -0300

    Refactor custom-buttom to use styled-components

--- crown-clothing/src/components/custom-button/custon-button.component.jsx
+++ crown-clothing/src/components/custom-button/custon-button.component.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
+import React from "react";
 
-import './custon-button.styles.scss'
+import { CustomButtonContainer } from "./custom-button.styles";
 
-
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-  <button
-    className={`
-      ${inverted ? 'inverted' : ''}
-      ${isGoogleSignIn ? 'google-sign-in' : ''}
-      custom-button`}
-    {...otherProps}
-  >
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>
     {children}
-  </button >
-)
+  </CustomButtonContainer>
+);
 
-export default CustomButton
+export default CustomButton;
