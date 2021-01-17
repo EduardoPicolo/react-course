2be09bf29002cf1bef6a994a9fc47a15a6e22b83
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-01-17 05:48:45 -0300

    205. Clear Cart on sign out with Saga

--- crown-clothing/src/redux/cart/cart.reducer.js
+++ crown-clothing/src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import { CartActionTypes } from './cart.types';
+import CartActionTypes from './cart.types';
 import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
@@ -8,33 +8,40 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case CartActionTypes.TOGGLE_CART_HIDDEN:
-      return {
-        ...state,
-        hidden: !state.hidden
-      }
+		case CartActionTypes.TOGGLE_CART_HIDDEN:
+			return {
+				...state,
+				hidden: !state.hidden
+			};
 
-    case CartActionTypes.ADD_ITEM:
+		case CartActionTypes.ADD_ITEM:
+			return {
+				...state,
+				cartItems: addItemToCart(state.cartItems, action.payload)
+			};
 
-      return {
-        ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
-      }
+		case CartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state,
+				cartItems: state.cartItems.filter(
+					(item) => item.id !== action.payload.id
+				)
+			};
 
-    case CartActionTypes.CLEAR_ITEM_FROM_CART:
-      return {
-        ...state,
-        cartItems: state.cartItems.filter((item => item.id !== action.payload.id))
-      }
+		case CartActionTypes.REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: removeItemFromCart(state.cartItems, action.payload)
+			};
 
-    case CartActionTypes.REMOVE_ITEM:
-      return {
-        ...state,
-        cartItems: removeItemFromCart(state.cartItems, action.payload)
-      }
+		case CartActionTypes.CLEAR_CART:
+			return {
+				...state,
+				cartItems: []
+			};
 
-    default:
-      return state;
+		default:
+			return state;
   }
 }
 
