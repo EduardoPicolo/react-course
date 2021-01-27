8a1ce85785632b4a5119316659691265b35b4480
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-01-27 19:25:06 -0300

    233. Context Consumer + useContext Hook

---
+++ react-context-lesson/src/pages/collection/collection.component.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionsContext from '../../contexts/collections/collections.context';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title,items } = collection
+
+	return (
+		<div className="collection-page">
+			<h2 className="title">{title}</h2>
+			<div className="items">
+				{items.map((item) => (
+					<CollectionItem key={item.id} item={item} />
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default CollectionPage;
