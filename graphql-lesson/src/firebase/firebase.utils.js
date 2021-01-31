ab757a89219052b376da67730a723e04b8805447
Eduardo Picolo <eduardonpicolo@gmail.com>
2021-01-30 22:03:11 -0300

    245. Introduction to Apollo

---
+++ graphql-lesson/src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+	apiKey: 'AIzaSyDWufeS7Rpdm6WNdDZE12nSjYxuTMSTCGk',
+	authDomain: 'crown-db-4c473.firebaseapp.com',
+	databaseURL: 'https://crown-db-4c473.firebaseio.com',
+	projectId: 'crown-db-4c473',
+	storageBucket: 'crown-db-4c473.appspot.com',
+	messagingSenderId: '593423769325',
+	appId: '1:593423769325:web:04f38bec2dd1f05c6f36e4',
+	measurementId: 'G-L56JE462RY'
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
---
+++ graphql-lesson/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
+
+import { store, persistor } from './redux/store';
+
+import './index.css';
+import App from './App';
+
+const httpLink = createHttpLink({
+	uri: 'https://crwn-clothing.com'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache
+});
+
+client.query({
+	query: gql`
+		{
+			collection(id: "cjwuuj5bz000i0719rrtw5gqk") {
+				title
+			}
+		}
+	`
+}).then(res => console.log(res))
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<Provider store={store}>
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
+		</Provider>
+	</ApolloProvider>,
+	document.getElementById('root')
+);
